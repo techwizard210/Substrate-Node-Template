56f61d3170128291f4cba3dcd57208be262a2db9
Worlddev <worlddev5007@gmail.com>
2022-06-08 00:46:11 +0900

    Changed pallet mock 2022.6.8

--- pallets/template/src/mock.rs
+++ pallets/template/src/mock.rs
@@ -1,11 +1,11 @@
-use crate as pallet_template;
-use frame_support::traits::{ConstU16, ConstU64};
-use frame_system as system;
-use sp_core::H256;
-use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
-};
+// use crate as pallet_template;
+// use frame_support::traits::{ConstU16, ConstU64};
+// use frame_system as system;
+// use sp_core::H256;
+// use sp_runtime::{
+// 	testing::Header,
+// 	traits::{BlakeTwo256, IdentityLookup},
+// };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
